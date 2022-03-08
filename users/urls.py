6c048acd0322890091a0697899782725d095b503
GraceMwende <grace.micheni@student.moringaschool.com>
2022-03-08 19:38:23 +0300

    add signup page

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
-from django.conf import settings
+# from django.conf import settings
 from django.conf.urls.static import static
+from .views import RegisterView
 
 urlpatterns = [
-  path('',views.home, name='users-home')
+  path('',views.home, name='users-home'),
+  path('register/',RegisterView.as_view(),name='users-register')
 ]
