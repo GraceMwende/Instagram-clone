c11e0f7c6fc5dae0bc2b5f29e68eacfb9f0e4a5e
GraceMwende <grace.micheni@student.moringaschool.com>
2022-03-08 16:20:03 +0300

    create models

--- users/models.py
+++ users/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+  user = models.OneToOneField(User, on_delete=models.CASCADE)
+  # profile_photo = models.ImageField(default='default.jpg',upload_to='media')
+  bio = models.TextField()
+
+  def __str__(self):
+        return self.user.username
+
+class Image(models.Model):
+  image = models.ImageField(upload_to='images/', null=False)
+  image_name = models.CharField(max_length=30)
+  image_caption = models.CharField(max_length=30)
+  profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
