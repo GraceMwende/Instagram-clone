21478e683cf903afaa2895b59f639d35405ee051
GraceMwende <grace.micheni@student.moringaschool.com>
2022-03-09 08:54:47 +0300

    add methods

--- users/models.py
+++ users/models.py
@@ -27,3 +27,17 @@ class Image(models.Model):
   image_name = models.CharField(max_length=30)
   image_caption = models.CharField(max_length=30)
   profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return self.image_name
+
+  def save_image(self):
+    self.save()
+
+  def delete_image(self):
+    self.delete()
+
+  @classmethod
+  def display_images(cls):
+    img = cls.objects.all()
+    return img
